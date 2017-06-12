995a14f25a8cc1b118ad35cbd56b928a364b050d
Joerg Bellmann <joerg.bellmann@zalando.de>
2017-06-12 21:45:27 +0200

    fix multiple 'AccessTokens'-instances

--- spring-boot-zalando-stups-tokens/src/main/java/org/zalando/stups/tokens/config/SecretsAutoConfiguration.java
+++ spring-boot-zalando-stups-tokens/src/main/java/org/zalando/stups/tokens/config/SecretsAutoConfiguration.java
@@ -1,9 +1,13 @@
 package org.zalando.stups.tokens.config;
 
+import static java.util.Objects.requireNonNull;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.stups.tokens.AccessTokensBean;
+import org.zalando.stups.tokens.ClientCredentials;
+import org.zalando.stups.tokens.Secret;
 import org.zalando.stups.tokens.Secrets;
 
 /**
@@ -17,7 +21,27 @@ public class SecretsAutoConfiguration {
 
     @Bean
     public Secrets secrets(AccessTokensBean accessTokensBean) {
-        return (Secrets) accessTokensBean.getDelegate();
+        return new SecretsAdapter((Secrets) accessTokensBean.getDelegate());
+    }
+
+    static class SecretsAdapter implements Secrets {
+
+        private Secrets delegate;
+
+        SecretsAdapter(Secrets delegate) {
+            this.delegate = requireNonNull(delegate, "'secrets'-delegate should never be null");
+        }
+
+        @Override
+        public Secret getSecret(String identifier) {
+            return delegate.getSecret(identifier);
+        }
+
+        @Override
+        public ClientCredentials getClient(String identifier) {
+            return delegate.getClient(identifier);
+        }
+
     }
 
 }
